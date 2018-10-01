1336318d20000fa5d97067a569680aecb24a775a
王思琪 <wangsiqi@WangSiqideMacBook-Pro.local>
2018-10-01 00:20:42 -0700

    first first commit

---
+++ app/src/main/java/com/example/wangsiqi/present/MainActivity0927.java
@@ -0,0 +1,37 @@
+package com.example.wangsiqi.present;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity0927 extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.wangsiqi.present.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main0927);
+    }
+
+    /** Called when the user taps the search_zip button */
+    public void searchZip(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, ZipcodeActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editZip);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the search_location button */
+    public void searchLocation(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, MapsActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editLocation);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/wangsiqi/present/ZipcodeActivity.java
@@ -0,0 +1,25 @@
+package com.example.wangsiqi.present;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ZipcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zipcode);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity0927.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.inputZip);
+        textView.setText(message);
+    }
+}
+
+
